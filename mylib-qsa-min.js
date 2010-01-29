5c51c8562ce54731c7e750027e13cdc47481ec8b
David Mark <dmark.cinsoft@gmail.com>
2010-01-29 16:18:21 +0000

    Optimizations for selector query "slow lane"
    git-svn-id: http://ourlibrary.googlecode.com/svn/trunk@8 fad0cf9c-0ac3-11df-8f06-1508ab885915

--- mylib-qsa-min.js
+++ mylib-qsa-min.js
@@ -30,3 +30,31 @@ var API,global=this;API=API||{};var $;var Q,E,D,F,W,I;(function(){var n,Cv;var P
 
 */
 
+// QSA add-on uses built-in queries
+var global = this;
+if (this.API && this.API.getEBCS && this.API.isHostMethod(this.document, 'querySelectorAll')) {
+	(function() {
+		var oldGetEBCS = this.API.getEBCS;
+		var toArray;
+
+		try {
+			(Array.prototype.slice.call(global.document.querySelectorAll('html'), 0));
+			toArray = function(a) {
+				return Array.prototype.slice.call(a, 0);
+			};
+		} catch(e) {
+			toArray = API.toArray;
+		}
+
+		this.API.getEBCS = function(s, d) {
+			try {
+				return toArray((d || global.document).querySelectorAll(s));
+			} catch(e) {
+				return oldGetEBCS(s, d);
+			}
+		};
+		if (typeof $ == 'function') {
+			$ = this.API.getEBCS;
+		}
+	})();
+}
