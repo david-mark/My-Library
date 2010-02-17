cacb6b2fbcb0b77d64eb079010a0a50e36517bbb
David Mark <dmark.cinsoft@gmail.com>
2010-02-17 04:04:55 +0000

    Added :empty and :enabled pseudo-selectors.  Removed :selected (doesn't exist in specs, but probably should).
    Added workarounds for QSA add-on for broken SELECTED attribute detection and inadequate empty node detection (not supposed to count comments or white-space-only text nodes).
    
    git-svn-id: http://ourlibrary.googlecode.com/svn/trunk@20 fad0cf9c-0ac3-11df-8f06-1508ab885915

--- mylib-qsa.js
+++ mylib-qsa.js
@@ -2,8 +2,42 @@
 var global = this;
 if (this.API && this.API.getEBCS && this.API.isHostMethod(this.document, 'querySelectorAll')) {
 	(function() {
-		var oldGetEBCS = this.API.getEBCS;
-		var toArray;
+		var api = global.API, oldGetEBCS = api.getEBCS;
+		var toArray, qsaEmptyInadequate, qsaOptionSelectedBroken;
+
+                var testEmpty = function(s, d, qsaCount) {
+                        var adequate, result = oldGetEBCS(s, d);
+			if (result.length == qsaCount) {
+				var doc = global.document, body = api.getBodyElement();
+				var el = api.createElement('div');
+				el.appendChild(doc.createTextNode(' '));
+				var count = doc.querySelectorAll('div:empty').length;
+				body.appendChild(el);
+				adequate = count == doc.querySelectorAll('div:empty').length - 1;
+				body.removeChild(el);
+			}
+			qsaEmptyInadequate = !adequate;
+			return result;
+		};
+
+		var testOptionSelected = function(s, d, qsaCount) {
+                        var adequate, result = oldGetEBCS(s, d);
+			if (result.length == qsaCount) {
+				var doc = global.document, body = api.getBodyElement();
+				var el = api.createElement('option');
+				el = api.setAttribute(el, 'selected');
+				var elFieldset = doc.createElement('fieldset');
+				var elSelect = doc.createElement('select');
+				elSelect.appendChild(el);
+				elFieldset.appendChild(elSelect);
+				var count = doc.querySelectorAll('option[selected]').length;
+				body.appendChild(elFieldset);
+				adequate = count == doc.querySelectorAll('option[selected]').length - 1;
+				body.removeChild(elFieldset);
+			}
+			qsaOptionSelectedBroken = !adequate;
+			return result;
+		};
 
 		try {
 			(Array.prototype.slice.call(global.document.querySelectorAll('html'), 0));
@@ -11,12 +45,27 @@ if (this.API && this.API.getEBCS && this.API.isHostMethod(this.document, 'queryS
 				return Array.prototype.slice.call(a, 0);
 			};
 		} catch(e) {
-			toArray = API.toArray;
+			toArray = api.toArray;
 		}
 
 		this.API.getEBCS = function(s, d) {
+			var result, newResult;
+
+			if (qsaEmptyInadequate && s.indexOf(':empty') != -1 || (qsaOptionSelectedBroken && s.indexOf('[selected]') != -1)) {
+				return oldGetEBCS(s, d);
+			}
+
 			try {
-				return toArray((d || global.document).querySelectorAll(s));
+				result = toArray((d || global.document).querySelectorAll(s));
+				if (typeof qsaEmptyInadequate == 'undefined' && s.indexOf(':empty') != -1) {
+					newResult = testEmpty(s, d, result.length);
+					return qsaEmptyInadequate ? newResult : result;
+				}
+				if (typeof qsaOptionSelectedBroken == 'undefined' && s.indexOf('[selected]') != -1) {
+					newResult = testOptionSelected(s, d, result.length);
+					return qsaOptionSelectedBroken ? newResult : result;
+				}
+				return result;
 			} catch(e) {
 				return oldGetEBCS(s, d);
 			}
